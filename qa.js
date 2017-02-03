65e604d36925ae65aab0310fc5ae2f503e678ee1
ctro <clint@ctro.net>
2017-02-02 18:14:13 -0700

    reorder q/a's with important stuff first.  cause help message

--- qa.js
+++ qa.js
@@ -4,11 +4,11 @@ module.exports = function QA () {
   // upload media to google's S3?: https://console.cloud.google.com/storage/browser?project=wired-victor-155621
   this.map = {
     // 'later': '<speak>Peace out river trout</speak>',
-    'tell an elk joke': '<speak>At what elevation do deer turn into elk?<break time="1"/>Typically around 8000 feet, but as low as 6000 feet in areas where the doe-zone layer has been depleted</speak>',
     'play the forecast': '<speak><audio src="https://www.mountainweather.com/forecast/forecast.mp3">MountainWeather Forecast</audio></speak>',
     'what\'s a good place for breakfast': '<speak>Try Persephone in Jackson or Nora\'s in Wilson.</speak>',
     'what\'s a good place for lunch': '<speak>Try Sweetwater or Snake River Brewing in Jackson </speak>',
-    'what\'s a good place for dinner': '<speak>Try Trio in Jackson</speak>'
+    'what\'s a good place for dinner': '<speak>Try Trio in Jackson</speak>',
+    'tell an elk joke': '<speak>At what elevation do deer turn into elk?<break time="1"/>Typically around 8000 feet, but as low as 6000 feet in areas where the doe-zone layer has been depleted</speak>'
   };
 
   // Build help message from keys
